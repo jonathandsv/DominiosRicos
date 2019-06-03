92a00ae14d34930e6996112adabeb208a5bec192
Jonathan de Souza Vieira <jhonansouza@hotmail.com>
2019-06-03 10:50:53 -0300

    atualizações

---
+++ PaymentContext.Domain/entities/Student.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Domain.Entities
+{
+  public class Student : Entity
+  {
+    private IList<Subscription> _subscriptions;
+    public Student(Name name, Document document, string email)
+    {
+      Name = name;
+      Document = document;
+      Email = email;
+      _subscriptions = new List<Subscription>();
+    }
+
+    public Name Name { get; private set; }
+    public Document Document { get; private set; }
+    public string Email { get; private set; }
+    public string Address { get; private set; }
+    public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+    public void AddSubscription(Subscription subscription)
+    {
+      foreach (var sub in Subscriptions)
+      {
+        sub.Inactivate();
+      }
+
+      _subscriptions.Add(subscription);
+    }
+  }
+
+}
